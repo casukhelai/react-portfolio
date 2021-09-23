741daf3ec3f5160cdf2a19552f05e60235dfce84
Ishya Casukhela <casukhelai@gmail.com>
2021-09-23 19:35:18 -0400

    creating form

--- app/src/components/Contact/index.js
+++ app/src/components/Contact/index.js
@@ -1,1 +1,30 @@
 // This will have forms => use hooks
+import React, { useState } from 'react';
+
+// contactForm is the functional component
+// 'callback' => the function that gets called when the user submits the form
+const contactForm = (callback) => {
+    //set the input fields, useState is a hook that initializes a state & setter function
+    const [field, setField] = useState({});
+
+    // function handles the submit event and prevents default of browser, and calls the 'callback' function
+    const handleSubmit = (event) => {
+        if(event){
+            event.preventDefault();
+        }
+        callback();
+    }
+
+    // handles when the user is inputting into the fields
+    const handleFieldChange = (event) => {
+        event.persist();
+        setField(field => ({...inputs, [event.target.name]: event.target.value}));
+    }
+    return {
+        handleSubmit,
+        handleFieldChange,
+        field
+    };
+}
+
+export default contactForm;

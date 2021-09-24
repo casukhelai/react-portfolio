2cc5a2b7afe5527533c21648cd19892f3cb11ab9
Ishya Casukhela <casukhelai@gmail.com>
2021-09-24 11:52:44 -0400

    creating first link in nav

---
+++ app/src/components/NavTabs.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function NavTabs({ currentPage, handlePageChange }) {
+    return (
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <a
+            href="#about"
+            onClick={() => handlePageChange('About')}
+            // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+            // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+            className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+          >
+            About
+          </a>
+        </li>
+        </ul>
+    );
+  }
+  
+  export default NavTabs;

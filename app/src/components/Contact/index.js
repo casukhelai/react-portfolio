ae6f069f00bd30ce001a848ee7f19196148d0018
Ishya Casukhela <casukhelai@gmail.com>
2021-09-23 21:45:31 -0400

    added visual element to page

--- app/src/components/Contact/index.js
+++ app/src/components/Contact/index.js
@@ -5,7 +5,7 @@ import React, { useState } from 'react';
 // 'callback' => the function that gets called when the user submits the form
 const contactForm = (callback) => {
     //set the input fields, useState is a hook that initializes a state & setter function
-    const [field, setField] = useState({});
+    const [field, setField] = useState({ userName: '', email: '', message: ''});
 
     // function handles the submit event and prevents default of browser, and calls the 'callback' function
     const handleSubmit = (event) => {
@@ -18,13 +18,33 @@ const contactForm = (callback) => {
     // handles when the user is inputting into the fields
     const handleFieldChange = (event) => {
         event.persist();
-        setField(field => ({...inputs, [event.target.name]: event.target.value}));
+        setField(field => ({...field, [event.target.userName]: event.target.value}));
     }
-    return {
-        handleSubmit,
-        handleFieldChange,
-        field
-    };
-}
+    return (
+        <section class='container'>
+            <h2 class='form-title'>Contact Form</h2>
+        <form class='justify-content-center' id='contact-form'>
+            <div>
+                <label htmlFor='userName'>Name:</label>
+                <input class='form-control' type='text' name='name' defaultValue={userName} onBlur={handleFieldChange}></input>
+            </div>
+            <div>
+                <label htmlFor='email'>Email:</label>
+                <input class='form-control' type='text' name='email' defaultValue={email} onBlur={handleFieldChange}></input>
+            </div>
+            <div>
+                <label htmlFor='message'>Message:</label>
+                <textarea class='form-control' type='text' name='message' defaultValue={message} onBlur={handleFieldChange}></textarea>
+            </div>
+
+            <div>
+                <button class='btn btn-outline-dark' type='submit' onSubmit={handleSubmit}>Submit</button>
+            </div>
+        </form>
+        </section>
+    )
+};
 
 export default contactForm;
+
+

d7717f99c04ba32e4807bf46c06447a883852d8a
Ishya Casukhela <casukhelai@gmail.com>
2021-09-23 22:38:48 -0400

    added an interim paragraph

--- app/src/components/About/index.js
+++ app/src/components/About/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-export default function About() {
+const About = () => {
   return (
-    <div class="containers">
+    <div class="container">
       <h1>About Me</h1>
       <p>
-        
+          Sint ea ipsum reprehenderit officia in. Exercitation mollit cillum mollit enim cillum voluptate est ea anim. 
+          Qui minim do dolore ut mollit do in id mollit ipsum. Aute tempor sint qui cupidatat irure minim ullamco ad pariatur.
       </p>
     </div>
   );
 }
+
+export default About();

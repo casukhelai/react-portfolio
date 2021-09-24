e1aee25e2cf5833aea1524a548236e86200a996d
Ishya Casukhela <casukhelai@gmail.com>
2021-09-23 22:38:24 -0400

    created app.js to view current process

--- app/src/App.js
+++ app/src/App.js
@@ -1,2 +1,27 @@
 import React from 'react';
+// import { HashRouter as Router, Route } from 'react-router-dom';
+import About from './components/About';
+import Contact from './components/Contact';
 import Header from './components/Header';
+import Footer from './components/Footer';
+// import Navigation from './components/Navigation';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <About/>
+            <Contact/>
+            <Portfolio/>
+            <Resume/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default App;
+
+
+

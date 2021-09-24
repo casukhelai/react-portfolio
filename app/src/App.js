1d458b8eaad9402494d048f517b1980f255c2bf1
Ishya Casukhela <casukhelai@gmail.com>
2021-09-24 12:09:38 -0400

    called portfoliocontainer instead of individual components

--- app/src/App.js
+++ app/src/App.js
@@ -1,27 +1,6 @@
-import React from 'react';
-// import { HashRouter as Router, Route } from 'react-router-dom';
-import About from './components/About';
-import Contact from './components/Contact';
-import Header from './components/Header';
-import Footer from './components/Footer';
-// import Navigation from './components/Navigation';
-import Portfolio from './components/Portfolio';
-import Resume from './components/Resume';
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-const App = () => {
-    return (
-        <div>
-            <Header/>
-            <About/>
-            <Contact/>
-            <Portfolio/>
-            <Resume/>
-            <Footer/>
-        </div>
-    )
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
-
-
-

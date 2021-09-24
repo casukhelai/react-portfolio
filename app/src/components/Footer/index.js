3a22db412868a5b3fd1831b272a9b25f3deeb53e
Ishya Casukhela <casukhelai@gmail.com>
2021-09-23 22:39:57 -0400

    created space for links

---
+++ app/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <footer>
+            <a href=''></a>
+            <a href=''></a>
+            <a href=''></a>
+        </footer>
+    )
+}
+export default Footer;

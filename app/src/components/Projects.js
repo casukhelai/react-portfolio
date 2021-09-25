9e4192761235abfa5c698d1a42030553ccbe697a
Ishya Casukhela <casukhelai@gmail.com>
2021-09-24 21:05:33 -0400

    created a dynamic card st it takes the props from the portfolio js and injects it into the portfolio page

--- app/src/components/Projects.js
+++ app/src/components/Projects.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 // create a function to hold project links
-const Projects = () => {
+const Projects = (prop) => {
     return(
-    <div className="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="..." />
+    <div className="card" style={{width: '18rem'}}>
+            <img src={prop.projInfo.image} class="card-img-top" alt="..." />
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                <h5 className="card-title">{prop.projInfo.title}</h5>
+                    <p className="card-text">{prop.projInfo.description}</p>
+                        <a href={prop.projInfo.app_link} className="btn btn-primary">View App!</a>
             </div>
         </div>
     )
 }
+
+export default Projects;

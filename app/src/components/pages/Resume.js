1bf64063bd6a6d4e3b56df6d06a29005772676f3
Ishya Casukhela <casukhelai@gmail.com>
2021-09-24 20:18:45 -0400

    created a template for resume

---
+++ app/src/components/pages/Resume.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+        <section>
+            <div className='center'>
+                <h1 className='page-header'>My Resume!</h1>
+            </div>
+        </section>
+    )
+}
+
+export default Resume;

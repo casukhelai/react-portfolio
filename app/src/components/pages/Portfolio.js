88d94d891801781e4b7efad5bc55c3db97d6bc85
Ishya Casukhela <casukhelai@gmail.com>
2021-09-24 20:18:20 -0400

    creating an object of github links

---
+++ app/src/components/pages/Portfolio.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Portfolio = () => {
+    const arr = [
+        {
+            title: ,
+            image: ,
+            link: '',
+            description: ''
+        }
+    ]
+}
+
+export default Portfolio;
